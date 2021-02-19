dc68624ee297c3da408234764d9da5787712a969
Ben M <bmeridet@depaul.edu>
2021-02-19 17:29:04 -0600

    added first complete version of project 1

--- Phase1_Project/FileMain.java
+++ Phase1_Project/FileMain.java
@@ -1,12 +1,18 @@
 package Phase1_Project;
 
+import java.util.Scanner;
+
 public class FileMain {
+
+    public static final Scanner scn = new Scanner(System.in);
 
     public static void main (String[] args)
     {
         FileSystem sys = new FileSystem("C:/Users/benja/projects/java/Phase1_Project/Data");
 
-        MainInterface.Run(sys);
+        MainInterface.RunMain(sys);
+
+        scn.close();
     }
     
 }
--- Phase1_Project/MainInterface.java
+++ Phase1_Project/MainInterface.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class MainInterface {
 
-    public static void Run(FileSystem sys)
-    {
-        Scanner scn = new Scanner(System.in);
+    private static Scanner scn = new Scanner(System.in);
 
+    public static void RunMain(FileSystem sys)
+    {
         Menu.DisplayMain();
         Integer input = scn.nextInt();
 
@@ -16,14 +16,20 @@ public class MainInterface {
             switch(input)
             {
                 case 1:
+                {
                     sys.PrintFileList();
                     break;
+                }
                 case 2:
-                    SubInterface.Run(sys);
+                {
+                    RunSub(sys);
                     break;
+                }
                 default:
+                {
                     System.out.println("Incorrect input.");
                     break;
+                }
             }
 
             Menu.DisplayMain();
@@ -31,8 +37,56 @@ public class MainInterface {
         }
 
         System.out.println("Exiting. Goodbye!");
-
         scn.close();
+    }
+
+    private static void RunSub(FileSystem sys)
+    {
+        Menu.DisplaySub();
+        Integer input = scn.nextInt();
+        String fName;
+
+        while (input != 4)
+        {
+            switch (input)
+            {
+                case 1:
+                {
+                    System.out.println("Enter file name for search: ");
+                    fName = scn.next();
+                    sys.FindFile(fName);
+
+                    break;
+                }
+                case 2:
+                {
+                    System.out.println("Enter file name to add: ");
+                    fName = scn.next();
+                    sys.AddFile(fName);
+
+                    break;
+                }
+                case 3:
+                {
+                    System.out.println("Enter file name to delete: ");
+                    fName = scn.next();
+                    sys.DeleteFile(fName);
+
+                    break;
+                }
+                case 4:
+                {
+                    System.out.println("Returning to main menu.");
+                    break;
+                }
+                default:
+                    break;
+            }
+
+            Menu.DisplaySub();
+            input = scn.nextInt();
+        }
+
     }
     
 }
