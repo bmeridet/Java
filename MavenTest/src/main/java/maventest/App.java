ba0c6ed05f1c04850ce72d2874dda9e7bbb4b2cc
Ben M <bmeridet@depaul.edu>
2021-03-07 09:44:54 -0600

    added edits to maventest

--- MavenTest/src/main/java/maventest/App.java
+++ MavenTest/src/main/java/maventest/App.java
@@ -4,7 +4,11 @@ import java.sql.*;
 
 public class App 
 {
-    public static void main( String[] args )
+    String qry;
+    Connection dbCon;
+    Statement stmt;
+
+    App()
     {
         try
         {
@@ -15,9 +19,9 @@ public class App
 
             Class.forName(mysqlDriver);
 
-            Connection dbCon = DriverManager.getConnection(urlConnect, usr, password);
+            dbCon = DriverManager.getConnection(urlConnect, usr, password);
 
-            Statement stmt = dbCon.createStatement();
+            stmt = dbCon.createStatement();
 
             System.out.println("Connection successful.");
         }
@@ -28,6 +32,105 @@ public class App
         catch(SQLException e)
         {
             System.out.println("Could not connect to DB: " + e.getMessage());
+        }
+    }
+    public static void main( String[] args )
+    {
+        App app = new App();
+
+        // test insert - works
+        //app.Insert("hello");
+
+        // test GetRecord - works
+        //app.GetRecord(1);
+        //app.GetRecord(2);
+
+        // test Update - works
+        //app.Update(2, "Hello World");
+
+        // test Delete - works
+        //app.Delete(2);
+    }
+
+    private void GetRecord(int id)
+    {
+        qry = "select * from testtable where testID = ?";
+
+        try
+        {
+            PreparedStatement P = dbCon.prepareStatement(qry);
+
+            P.setInt(1, id);
+
+            ResultSet r = P.executeQuery();
+
+            while (r.next())
+            {
+                System.out.println("testName = " + r.getString("testName"));
+            }
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Can't get reference to prepared statement.");
+        }
+    }
+
+    private void Insert(String name)
+    {
+        qry = "insert into testtable(testName) values (?)";
+
+        try
+        {
+            PreparedStatement P = dbCon.prepareStatement(qry);
+
+            P.setString(1, name);
+
+            if (P.executeUpdate() > 0)
+                System.out.println("Entry added to DB.");
+
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Can't get reference to prepared statement: " + e.getMessage());
+        }
+    }
+
+    private void Update(int id, String name)
+    {
+        qry = "update testtable set testName = ? where testID = ?";
+
+        try
+        {
+            PreparedStatement P = dbCon.prepareStatement(qry);
+
+            P.setString(1, name);
+            P.setInt(2, id);
+
+            if (P.executeUpdate() > 0)
+                System.out.println("Record successfully updated.");
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Can't get reference to prepared statement: " + e.getMessage());
+        }
+    }
+
+    private void Delete(int id)
+    {
+        qry = "delete from testtable where testID = ?";
+
+        try
+        {
+            PreparedStatement P = dbCon.prepareStatement(qry);
+
+            P.setInt(1, id);
+
+            if (P.executeUpdate() > 0)
+                System.out.println("Record successfully deleted.");
+        }
+        catch(SQLException e)
+        {
+            System.out.println("Can't get reference to prepared statment: " + e.getMessage());
         }
     }
 }
